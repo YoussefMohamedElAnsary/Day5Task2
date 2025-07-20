eee42a63095fa6774e47f2f2c2de1f712dae72d2
Youssef ElAnsary <youssefmohamednbis@gmail.com>
2025-07-20 15:13:04 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ItemCardComponent } from './item-card/item-card.component'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ItemCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
